9178c50a0911d32b4519c6ee738a63e205c7afe0
Alex Megremis <alex.public@alexmegremis.com>
2020-07-09 20:15:14 +0100

    Initial commit

---
+++ src/main/java/com/alexmegremis/encryptdemo/CredentialsManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alexmegremis.encryptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredentialsManagementDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CredentialsManagementDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexmegremis/encryptdemo/CredentialsManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexmegremis.encryptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredentialsManagementDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
